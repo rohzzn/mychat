6986a99aeae266f3bd8fe63a343a9e13fb3a57ce
Rohan <rohansanjeev6464@gmail.com>
2023-08-03 18:51:23 +0530

    commit

--- mychat/wsgi.py
+++ mychat/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mychat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django.settings')
 
 application = get_wsgi_application()
 
